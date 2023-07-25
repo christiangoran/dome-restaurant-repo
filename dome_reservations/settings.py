96e25c6ff5bcfdb17186b0c4c999aa3f7c082449
Christian Göran <christian.goran@protonmail.com>
2023-07-25 19:45:00 +0000

    Add allowed host and updated STATIC_MEDIA in settings.py

--- dome_reservations/settings.py
+++ dome_reservations/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['dome-restaurant-hero.herokuapp.com', 'dome-restaurant-hero-9071346b8ec2.herokuapp.com']
+ALLOWED_HOSTS = ['dome-restaurant-hero.herokuapp.com', 'dome-restaurant-hero-9071346b8ec2.herokuapp.com', '8000-christiango-domerestaur-b4alb8kbqmj.ws-eu102.gitpod.io']
 
 
 # Application definition
@@ -134,7 +134,7 @@ STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-MEDIA_URL = '/static/'
+MEDIA_URL = '/media/'
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
 # Default primary key field type
