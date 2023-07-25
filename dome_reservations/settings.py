b62d8c1d47694d9f52ecad0c45ac7044f1be3077
Christian Göran <christian.goran@protonmail.com>
2023-07-25 05:17:55 +0000

    Update ALLOWED_HOSTS in settings.py

--- dome_reservations/settings.py
+++ dome_reservations/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['dome-restaurant-hero.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['dome-restaurant-hero.herokuapp.com', 'dome-restaurant-hero-9071346b8ec2.herokuapp.com']
 
 
 # Application definition
