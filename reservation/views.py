08bc4bb3ee77c3c237461073cd6c96d23e626052
christiangoran <christian.goran@protonmail.com>
2023-07-31 09:47:10 +0200

    Add basic Django views for Reservation and some templates

--- reservation/views.py
+++ reservation/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
+from django.views import generic
+from django.utils import timezone
 
-# Create your views here.
+from .models import Reservation, Table
+
+class IndexReservation(generic.ListView):
+    template_name = 'reservations/index.html'
+    context_object_name = 'latest_reservation_list'
+
+    def get_queryset(self):
+        return Reservation.objects.filter(date__lte=timezone.now()).order_by('-date')
+
+class DetailReservation(generic.DetailView):
+    model = Reservation
+    template_name = 'reservations/detail.html'
+
+class CreateReservation(generic.edit.CreateView):
+    model = Reservation
+    fields = ['user', 'date', 'start_time', 'end_time', 'notes', 'table', 'number_of_guests', 'image']
+    template_name = 'reservations/create.html'
+
+class UpdateReservation(generic.edit.UpdateView):
+    model = Reservation
+    fields = ['user', 'date', 'start_time', 'end_time', 'notes', 'table', 'number_of_guests', 'image']
+    template_name = 'reservations/update.html'
+
+class DeleteReservation(generic.edit.DeleteView):
+    model = Reservation
+    success_url = reverse_lazy('reservations:index')
+    template_name = 'reservations/delete.html'
