727fc0aa70e74bdbf8583d10867cfd1177fccdf4
christiangoran <christian.goran@protonmail.com>
2023-07-31 13:06:38 +0200

    Update Table and Reservation models

--- dome_reservations/urls.py
+++ dome_reservations/urls.py
@@ -19,5 +19,4 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('reservation.urls')),
-    path('details/', include('reservation.urls')),
 ]
--- reservation/models.py
+++ reservation/models.py
@@ -5,13 +5,13 @@ from cloudinary.models import CloudinaryField
 class Reservation(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
+    customer_email = models.EmailField(max_length=150, blank=True, null=True)
     date = models.DateField()
     time = models.TimeField()
     end_time = models.TimeField()
     notes = models.TextField(null=True, blank=True)
     table = models.ForeignKey('Table', on_delete=models.CASCADE)
     number_of_guests = models.IntegerField()
-    image = CloudinaryField('image', blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
@@ -20,7 +20,7 @@ class Reservation(models.Model):
 
 class Table(models.Model):
     table_number = models.IntegerField(unique=True)
-    number_of_seats = models.IntegerField()
+    number_of_seats = models.IntegerField(default=2)
 
     def __str__(self):
         return str(self.table_number)
