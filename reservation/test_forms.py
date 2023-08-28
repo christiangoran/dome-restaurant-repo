fb99b3d6bcc480c498a2a031902b8e496b34807a
christiangoran <christian.goran@protonmail.com>
2023-08-28 10:10:20 +0200

    Add 2 more tests to test_forms.py

--- reservation/test_forms.py
+++ reservation/test_forms.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
 from django.contrib.auth.models import User
-from .models import Table, Reservation, BOOKING_TIME
+from .models import Table
 from .forms import ReservationForm
 import datetime
 
@@ -25,3 +25,28 @@ class ReservationFormTestCase(TestCase):
         })
         self.assertTrue(form.is_valid())
 
+
+    def test_past_date(self):
+        form = ReservationForm({
+            'name': 'Testy McTesterson',
+            'customer_email': 'testy.mctesterson@cool.com',
+            'date': datetime.date.today() - datetime.timedelta(days=1),
+            'time': 1,  # same as "12:00pm - 1:45pm" on website
+            'notes': 'This is a test',
+            'number_of_guests': 2,
+        })
+        self.assertFalse(form.is_valid())
+        self.assertEqual(form.errors, {'__all__': ['Sorry you have to book a future date']})
+
+
+    def test_invalid_number_of_guests(self):
+        form = ReservationForm({
+            'name': 'Testy McTesterson',
+            'customer_email': 'testy.mctesterson@cool.com',
+            'date': datetime.date.today() + datetime.timedelta(days=1),
+            'time': 1,  # same as "12:00pm - 1:45pm" on website
+            'notes': 'This is a test',
+            'number_of_guests': 20,
+        })
+        self.assertFalse(form.is_valid())
+        self.assertEqual(form.errors, {'__all__': ['Sorry we do not have a table with that capacity available']})
